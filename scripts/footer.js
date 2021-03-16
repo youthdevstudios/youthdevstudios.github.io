f607de6553faa342b06b57b20d9d6fb45b409e9f
youthdevstudios <80779530+YouthDev-Studios@users.noreply.github.com>
2021-03-16 10:15:07 -0700

    Add files via upload

---
+++ scripts/footer.js
@@ -0,0 +1,5 @@
+document.write('
+  <p>The Youthdev name, logo, product names and logos, are trademarks of the company.</p>
+  <p>©' + new Date().getFullYear();' Youthdev Studios LLC</p>
+  <a href="©" id="footer-text"></a>
+');
