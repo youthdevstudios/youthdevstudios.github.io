e41ea54ea3f3d7bc67f9c99234bde7b879bc98ac
YD Studios <80779530+ydstudios@users.noreply.github.com>
2021-04-09 16:03:14 -0700

    Update navbar.js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('body').append('<header id="navbar"></header>','<nav></nav>','<ul></ul>')
-  $('body ul').append('<li></li>','<a href="index.html" class="hover">Home</a>')
-  $('body ul').append('<li></li>','<a href="index.html" class="hover">Games</a>')
-  $('body ul').append('<li></li>','<a href="index.html" class="hover">News</a>')
-  $('body ul').append('<li></li>','<a href="index.html" class="hover">Contact</a>')
+        $('body ul').append('<li></li>','<a href="index.html" class="hover">Home</a>')
+        $('body ul').append('<li></li>','<a href="index.html" class="hover">Games</a>')
+        $('body ul').append('<li></li>','<a href="index.html" class="hover">News</a>')
+        $('body ul').append('<li></li>','<a href="index.html" class="hover">Contact</a>')
 }
