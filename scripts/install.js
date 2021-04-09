7d41658099a2a2f40a966deeb308574e170d1331
YD Studios <80779530+ydstudios@users.noreply.github.com>
2021-04-09 16:05:08 -0700

    Delete install.js

--- scripts/install.js
+++
@@ -1,9 +0,0 @@
-document.write('<link rel="icon" href="icon.png');
-// Title font
-document.write('<link href="https://fonts.googleapis.com/css?family=Roboto:100&display=swap" rel="stylesheet">');
-// Display font
-document.write('<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">');
-// Serif font
-document.write('<link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro&display=swap" rel="stylesheet">');
-// jQuery
-document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js" />');
