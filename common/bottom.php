9fa107e49a8f5ca7d958a28ff6f4fb2b0668be54
juicetanz <juicetanz@gmail.com>
2021-10-10 13:55:23 -0700

    Fix 2.0

--- common/bottom.php
+++
@@ -1,6 +0,0 @@
-  </main>
-  <footer>
-    ©Copyright <?php echo date('Y'); ?> YD Studios, LLC. All rights reserved.
-  </footer>
-</body>
-</html>
--- games.php
+++
@@ -1,9 +0,0 @@
-<?php
-  include_once 'private/common/initialization.php';
-  $page_title = 'Home';
-  include_once 'common/top.php';
-?>
-
-<?php
-  include_once 'common/bottom.php';
-?>
