d7b843152cfb87d7638b609c07f89da92634442e
Notebooked <creepinglion09@gmail.com>
2022-12-05 12:16:02 -0800

    updat nam

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
 document.write(`
 <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/">Aquaticore</a>
+        <a class="navbar-brand" href="/">Youthdevs</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
