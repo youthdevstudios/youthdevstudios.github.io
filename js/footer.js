9d9c9b0c6b0fb6a7efb5b9ecfe1eff437ca1e988
Notebooked <creepinglion09@gmail.com>
2022-12-05 10:57:03 -0800

    FIXED

--- js/footer.js
+++ js/footer.js
@@ -1,1 +1,5 @@
-document.write('<footer><a href="mailto:business.youthdevs@gmail.com">business.youthdevs@gmail.com</a><p>Copyright &copy; 2022 Youthdevs, LLC. All rights reserved.</p></footer>')
+document.write(`
+<footer>
+    <a href="mailto:business.youthdevs@gmail.com">business.youthdevs@gmail.com</a>
+    <p>Copyright &copy; 2022 Youthdevs, LLC. All rights reserved.</p>
+</footer>`)
