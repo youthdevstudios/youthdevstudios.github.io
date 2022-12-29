6f53f8a5b67285ff6f87c35ffb48bb3628e026fd
juicetanz <66227184+juicetanz@users.noreply.github.com>
2022-12-29 08:58:54 -0500

    auto update footer year

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,6 @@
 document.write(`
-<footer>
-    <a href="mailto:business.youthdevs@gmail.com">business.youthdevs@gmail.com</a>
-    <p>Copyright &copy; 2022 Youthdevs, LLC. All rights reserved.</p>
-</footer>`)
+    <footer>
+        <a href="mailto:business.youthdevs@gmail.com">business.youthdevs@gmail.com</a>
+        <p>Copyright &copy; <script>document.write(new Date().getFullYear())</script> Youthdevs, LLC. All rights reserved.</p>
+    </footer>
+`)
